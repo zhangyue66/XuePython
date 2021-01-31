4ce74cbdb5ef195ee0578819e3590b6f0126b048
Yue Zhang <zhangyue19890116@gmail.com>
2021-01-30 20:39:56 -0800

    oop

---
+++ classDef.py
@@ -0,0 +1,16 @@
+#经典类
+#新式类
+
+class Money:
+    pass
+
+print(Money.__name__)
+#实例化 instantiate
+rmb = Money()
+
+dollar = Money()
+
+print(rmb,dollar)
+
+print(rmb.__class__)
+
---
+++ property.py
@@ -0,0 +1,11 @@
+class Person:
+    pass
+
+
+p = Person()
+
+#add property
+p.age = 18
+
+#verify if age added
+print(p.age)
