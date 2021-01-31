010682be98b07ac1d15a0e0ea87f65c6be776b7c
Yue Zhang <zhangyue19890116@gmail.com>
2021-01-30 18:26:38 -0800

    decorrator 0.2

--- decorator.py
+++ decorator.py
@@ -3,20 +3,55 @@
 # f = now
 # print(now.__name__,f.__name__)
 
-def log(func):
-    def wrapper(*args,**kwargs):
-        print("call %s():" % func.__name__)
-        return func(*args,**kwargs)
+# def log(func):
+#     def wrapper(*args,**kwargs):
+#         print("call %s():" % func.__name__)
+#         return func(*args,**kwargs)
+#
+#     return wrapper
+#
+#
+#
+#
+# @log
+# def  now():
+#
+#     print("2021-01-29")
+#
+# now()
 
-    return wrapper
+import functools
 
+def log(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args,**kwargs):
+            print("%s %s():" % (text,func.__name__))
+            return func(*args,**kwargs)
 
+        return wrapper
+    return decorator
 
-
-@log
-def  now():
-
-    print("2021-01-29")
+@log("execute")
+def now():
+    print("2020-01-30")
 
 now()
+print(now.__name__)  # function now's name changed to wrapper . not now anymore . introduce fucntools.wraps
+
+#before
+
+# execute now():
+# 2020-01-30
+# wrapper
+
+# after introduce wraps(func)
+
+# execute now():
+# 2020-01-30
+# now
+
+
+#########Exercise#######################
+
 
