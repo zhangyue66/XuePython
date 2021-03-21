7cfa4d7727d6b78e0a1adc2f2bf1e2fad9895e8a
Yue Zhang <zhangyue19890116@gmail.com>
2021-03-21 00:14:21 -0700

    封装

--- PythonOOP三大特性/inheritance.py
+++ PythonOOP三大特性/inheritance.py
@@ -68,8 +68,87 @@ class Person(Animal):
 p = Person()
 p.test()
 
-Animal.a =666
+Animal.a = 666
 p.test()
 
 
 # 三种继承形态 单继承 无重叠多继承 有重叠多继承  （资源的使用）
+
+# 继承标准方案的演化 python3.X之后 MRO DFS BFS
+
+# 继承资源的访问顺序
+
+#单继承
+class C:
+    age = "c"
+
+class B(C):
+    #age = "b"
+    pass
+
+class A(B):
+    #age = "a"
+    pass
+
+print(A.age)
+
+#无重叠多继承
+
+#A -B -C -D- E
+
+
+#有重叠多继承 如果再用dfs 会产生顺序错误 违背了重写可用的原则（python2.X）
+
+# import inspect
+# print(inspect.getmro(A))
+
+
+#python 2.2 MRO 原则 1.经典类 dfs 2.新式类 dfs改进 并不是bfs、
+
+
+# 此方法无法侦测有问题的继承 所以新式类引入C3算法
+
+#--------python 2.3 - 2.7的C3 linearization algorithm
+
+class D:
+    pass
+
+class B(D):
+    pass
+
+class C(D):
+    pass
+
+class A(B,C):
+    pass
+
+import inspect
+print(inspect.getmro(A))
+print("yz")
+
+
+#------python3.X之后没有经典类 只有C3 算法
+
+
+#------继承  资源的覆盖 -----
+
+
+class D:
+    pass
+
+class C(D):
+
+    age = "c"
+    pass
+
+class B(D):
+    age = "b"
+    pass
+
+class A(B,C):
+    pass
+
+print(A.mro())
+print(A.__mro__)
+print(A.age)  #优先调用优先级高的类里面  属性的覆盖 方法的重写
+
